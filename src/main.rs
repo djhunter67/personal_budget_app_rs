23db4d80d21faa4f201a34af58e0bc8efc8bd6b6
Christerpher Hunter <djhunter67@gmail.com>
2023-08-30 13:07:22 -0400

    add: main init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+use simplelog::{CombinedLogger, TermLogger, WriteLogger};
+use std::fs::File;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            log::LevelFilter::Debug,
+            simplelog::Config::default(),
+            simplelog::TerminalMode::Mixed,
+            simplelog::ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            simplelog::LevelFilter::Info,
+            simplelog::Config::default(),
+            File::create("my_rust_binary.log").unwrap(),
+        ),
+    ])
+    .unwrap();
+
+    let _ = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/health", web::get().to(health))
+    })
+    .bind(("127.0.0.1", 8000))
+    .unwrap_or_else(|_| {
+        log::warn!("Can not bind to port 8000");
+        std::process::exit(1);
+    })
+    .run()
+    .await;
+
+    Ok(())
+}
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("OK")
 }
